334de12be478469993d20bd26b85a7b91094034e
renanlisboa <renlp89.developer@gmail.com>
2022-05-10 23:03:17 -0300

    chore: add express, cors, helmet, morgan and ts-node-dev

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
